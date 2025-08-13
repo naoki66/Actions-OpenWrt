50fd67ade5f61d3d88bca3b4e794c7f944dc568a
yuayu <naoki66@163.com>
2025-08-13 13:35:10 +0800

    add OpenWrt-momo to lede

--- lede-diy-part1.sh
+++ lede-diy-part1.sh
@@ -9,6 +9,6 @@
 # Add a feed source 添加额外软件包
 #echo 'src-git helloworld  https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
 #echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
-#echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
+echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
 #echo 'src-git wechatpush  https://github.com/tty228/luci-app-wechatpush.git' >>feeds.conf.default
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
