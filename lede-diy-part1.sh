5a9050191698e7ca7a87674148122db5b4866fe0
yuayu <naoki66@163.com>
2025-08-13 16:21:52 +0800

    Update lede-diy-part1.sh

--- lede-diy-part1.sh
+++ lede-diy-part1.sh
@@ -7,7 +7,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source 添加额外软件包
-#echo 'src-git helloworld  https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
 echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
 #echo 'src-git wechatpush  https://github.com/tty228/luci-app-wechatpush.git' >>feeds.conf.default
