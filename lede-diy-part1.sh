91aed3694afda4186879a728852b7936b8983395
yuayu <naoki66@163.com>
2025-08-25 10:38:03 +0800

    Update lede-diy-part1.sh

--- lede-diy-part1.sh
+++ lede-diy-part1.sh
@@ -5,9 +5,12 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^src-git luci .*openwrt-23\.05$/#&/' feeds.conf.default
+sed -i 's/^#src-git luci .*openwrt-24\.10$/src-git luci https:\/\/github\.com\/coolsnowwolf\/luci\.git;openwrt-24\.10/' feeds.conf.default
+
 
 # Add a feed source 添加额外软件包
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
 #echo 'src-git wechatpush  https://github.com/tty228/luci-app-wechatpush.git' >>feeds.conf.default
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
