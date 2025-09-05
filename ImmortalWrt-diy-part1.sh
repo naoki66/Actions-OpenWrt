b6070de96ce9b463b9dfc70181a64d432af1ef6e
naoki66 <naoki66@163.com>
2025-09-05 22:31:12 +0800

    feat: 添加nikki软件源并启用相关配置
    - 在feeds.conf.default中添加nikki软件源
    - 在配置文件中启用nikki相关选项和中文语言包
    - 启用yq工具包

--- ImmortalWrt-diy-part1.sh
+++ ImmortalWrt-diy-part1.sh
@@ -7,4 +7,6 @@
 # Add a feed source
 #echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+
 
