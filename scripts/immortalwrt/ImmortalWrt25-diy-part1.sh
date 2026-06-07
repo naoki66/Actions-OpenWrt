78164cd6ba99c84ea675f80115e8aa92db8a9e05
yuayu <naoki66@163.com>
2026-06-07 23:15:52 +0800

    chore(immortalwrt): 调整软件包配置，添加mosdns和rtp2httpd支持
    1. 注释并替换原有lucky源拉取逻辑，直接克隆luci-app-lucky
    2. 添加rtp2httpd软件源
    3. 移除旧的mosdns和v2ray-geodata，新增sbwml的luci-app-mosdns仓库
    4. 启用lyaml、mosdns、luci-app-mosdns、v2dat等相关配置项
    5. 清理部分冗余的禁用配置

--- scripts/immortalwrt/ImmortalWrt25-diy-part1.sh
+++ scripts/immortalwrt/ImmortalWrt25-diy-part1.sh
@@ -5,8 +5,13 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
-#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+#添加lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#添加rtp2httpd
 echo "src-git rtp2httpd https://github.com/stackia/rtp2httpd.git;main" >> "feeds.conf.default"
 
+#更新luci-app-mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
